2cdad6e4fae0eb5141a62051d18dde401e540c94
Loza <gebremar@usc.edu>
2023-10-17 17:27:04 -0700

    fire ignition + one time fire use updates

--- prototype_6/Assets/Scripts/character_push.cs
+++ prototype_6/Assets/Scripts/character_push.cs
@@ -8,6 +8,7 @@ public class character_push : MonoBehaviour
     [SerializeField]
     private float forceMagnitude;
     private bool OnFire;
+    [SerializeField] private GameObject TinyFire;
 
     private float xInput, yInput;
     [SerializeField] private float moveSpeed = 0.01f;
@@ -73,10 +74,56 @@ public class character_push : MonoBehaviour
             if (collision.gameObject.CompareTag("grass") && OnFire)
             {
                 //destroy block
-                Destroy(collision.gameObject);
+                //Destroy(collision.gameObject);
+                IgniteAdjacentGrassBlocks(collision.gameObject.transform.position);
+
             }
         }
     }
+
+    private void IgniteAdjacentGrassBlocks(Vector3 position)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, 1f); 
+        foreach (Collider collider in colliders)
+        {
+            if (collider.gameObject.CompareTag("grass"))
+            {
+                // Ignite the adjacent grass block
+                Debug.Log("ignited adjacent grass block with fire");
+                GameObject fireAnimation = Instantiate(fireFXGO, collider.transform.position, Quaternion.identity);
+                StartCoroutine(DestroyAfterDelay(fireAnimation, 2f));
+                StartCoroutine(DestroyColliderAfterDelay(collider.gameObject, 2f)); // Destroy the collider object after 3 seconds
+                StartCoroutine(DestroyAfterIgnition(2f));
+                //Destroy(collider.gameObject);
+
+            }
+        }
+    }
+
+    private IEnumerator DestroyAfterIgnition(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        OnFire = false;
+        fireFXGO.SetActive(false);
+    }
+    private IEnumerator DestroyAfterDelay(GameObject objectToDestroy, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        // Turn off the fire 
+        var particleSystem = objectToDestroy.GetComponent<ParticleSystem>();
+        if (particleSystem != null)
+        {
+            particleSystem.Stop();
+        }
+        Destroy(objectToDestroy);
+    }
+
+    private IEnumerator DestroyColliderAfterDelay(GameObject colliderObject, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(colliderObject);
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -85,6 +132,7 @@ public class character_push : MonoBehaviour
             Debug.Log("Collision with fire");
             OnFire = true;
             fireFXGO.SetActive(true);
+            TinyFire.SetActive(false);
         }
     }
 
