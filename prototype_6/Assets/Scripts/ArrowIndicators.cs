459d56e02ba64debc3b4e8264dd61b07c1ecc51b
Josh Lee <josh_lee247@yahoo.com>
2023-11-14 12:48:11 -0800

    updated arrow indicators to player

---
+++ prototype_6/Assets/Scripts/ArrowIndicators.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowIndicators : MonoBehaviour
+{
+    [SerializeField] private GameObject[] arrowIndicatorArr;
+    [SerializeField] public LayerMask boundaryLayer;
+    private RaycastHit hit;
+    private character_push playerPushScript;
+
+        private void Start() {
+            playerPushScript = FindObjectOfType<character_push>();
+        }
+
+        private void Update()
+        {
+            if (!playerPushScript.isMoving) {
+                UpdateArrowIndicator();
+            } else {
+                CloseAllArrowIndicators();
+            }
+        }
+
+        private void SetArrowIndicatorBasedOnPlayerPos()
+        {
+            Vector3 playerPushDirection = transform.position - hit.transform.position;
+            playerPushDirection.y = 0;
+            Debug.Log(playerPushDirection);
+            UpdateArrowIndicator();
+        }
+
+        public void UpdateArrowIndicator()
+        {
+            if (!Physics.Raycast(transform.position, new Vector3(1f, 0f, 0f), out hit, 1, boundaryLayer)) {
+                // Debug.Log(hit.collider.gameObject.name);
+                arrowIndicatorArr[0].SetActive(true);
+            }
+            if (!Physics.Raycast(transform.position, new Vector3(-1f, 0f, 0f), out hit, 1, boundaryLayer)) {
+                // Debug.Log(hit.collider.gameObject.name);
+                arrowIndicatorArr[1].SetActive(true);
+            }
+            if (!Physics.Raycast(transform.position, new Vector3(0f, 0f, -1f), out hit, 1, boundaryLayer)) {
+                // Debug.Log(hit.collider.gameObject.name);
+                arrowIndicatorArr[2].SetActive(true);
+            }
+            if (!Physics.Raycast(transform.position, new Vector3(0f, 0f, 1f), out hit, 1, boundaryLayer)) {
+                // Debug.Log(hit.collider.gameObject.name);
+                arrowIndicatorArr[3].SetActive(true);
+            }
+        }
+
+        public void CloseAllArrowIndicators()
+        {
+            for (int i = 0; i < arrowIndicatorArr.Length; i++)
+            {
+                arrowIndicatorArr[i].SetActive(false);
+            }
+        }
+}
--- prototype_6/Assets/Scripts/PushableBlock.cs
+++ prototype_6/Assets/Scripts/PushableBlock.cs
@@ -26,13 +26,13 @@ namespace ns
         private void Start()
         {
             playerPushScript = FindObjectOfType<character_push>();
-            playerPushScript.OnMoveFinishedHandler += CheckPlayerAndSetArrowIndicator;
-            CheckPlayerAndSetArrowIndicator();
+            // playerPushScript.OnMoveFinishedHandler += CheckPlayerAndSetArrowIndicator;
+            // CheckPlayerAndSetArrowIndicator();
         }
 
         private void OnDestroy()
         {
-            playerPushScript.OnMoveFinishedHandler -= CheckPlayerAndSetArrowIndicator;
+            // playerPushScript.OnMoveFinishedHandler -= CheckPlayerAndSetArrowIndicator;
         }
 
         public void Move(Vector3 direction)
@@ -53,73 +53,6 @@ namespace ns
 			}
 			transform.position = target;
 		}
-
-        public void CheckPlayerAndSetArrowIndicator()
-        {
-            if (Physics.Raycast(transform.position, Vector3.right, out hit, 1, LayerMask.NameToLayer("Player")))
-            {
-                SetArrowIndicatorBasedOnPlayerPos();
-                Debug.Log("?????");
-            }
-            if (Physics.Raycast(transform.position, Vector3.left, out hit, 1, LayerMask.NameToLayer("Player")))
-            {
-                SetArrowIndicatorBasedOnPlayerPos();
-                Debug.Log("111111");
-            }
-            if (Physics.Raycast(transform.position, Vector3.forward, out hit, 1, LayerMask.NameToLayer("Player")))
-            {
-                SetArrowIndicatorBasedOnPlayerPos();
-                Debug.Log("2222222");
-            }
-            if (Physics.Raycast(transform.position, Vector3.back, out hit, 1, LayerMask.NameToLayer("Player")))
-            {
-                SetArrowIndicatorBasedOnPlayerPos();
-                Debug.Log("333333333");
-            }
-        }
-
-        private void SetArrowIndicatorBasedOnPlayerPos()
-        {
-            Vector3 playerPushDirection = transform.position - hit.transform.position;
-            playerPushDirection.y = 0;
-            Debug.Log(playerPushDirection);
-            UpdateArrowIndicator(playerPushDirection);
-        }
-
-        public bool UpdateArrowIndicator(Vector3 pushDirection)
-        {
-            if (Physics.Raycast(transform.position, pushDirection, 1, boundaryLayer)) return false;
-
-           
-            CloseAllArrowIndicators();
-
-            if (pushDirection == Vector3.left) {
-                arrowIndicatorArr[0].SetActive(true);
-                gameObject.GetComponentInChildren<Outline>().enabled = true;
-            }
-            else if (pushDirection == Vector3.right) {
-                arrowIndicatorArr[1].SetActive(true);
-                gameObject.GetComponentInChildren<Outline>().enabled = true;
-            }
-            else if (pushDirection == Vector3.back) {
-                arrowIndicatorArr[2].SetActive(true);
-                gameObject.GetComponentInChildren<Outline>().enabled = true;
-            }
-            else if (pushDirection == Vector3.forward) {
-                arrowIndicatorArr[3].SetActive(true);
-                gameObject.GetComponentInChildren<Outline>().enabled = true;
-            }
-
-            return true;
-        }
-
-        public void CloseAllArrowIndicators()
-        {
-            for (int i = 0; i < arrowIndicatorArr.Length; i++)
-            {
-                arrowIndicatorArr[i].SetActive(false);
-            }
-        }
 
         private void OnDrawGizmos()
         {
