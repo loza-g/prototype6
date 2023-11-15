f80d083d65662199fe1ccf623b73958092bb0e4a
ShenceMYX <71965322+ShenceMYX@users.noreply.github.com>
2023-11-15 15:58:17 +0800

    update levels

--- prototype_6/Assets/Scripts/vfx/RainAreaPlacer.cs
+++ prototype_6/Assets/Scripts/vfx/RainAreaPlacer.cs
@@ -11,10 +11,11 @@ namespace ns
 	{
 		[SerializeField] private Transform rainAreaTrans;
 		private RaycastHit hit;
+		[SerializeField] private LayerMask rayCastLayers;
 
         private void Update()
         {
-            if(Physics.Raycast(transform.position, Vector3.down, out hit, 4))
+            if(Physics.Raycast(transform.position, Vector3.down, out hit, 4, rayCastLayers))
 			{
 				rainAreaTrans.position = hit.point;
 			}
