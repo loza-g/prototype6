f75b24c620573eb48088268148b4b60f4818a723
Josh Lee <joshtlee@usc.edu>
2023-11-27 15:58:45 -0800

    added level selection menu

---
+++ prototype_6/Assets/Scripts/LevelSelectionMenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionMenuManager : MonoBehaviour
+{
+    public static int currLevel;
+    public void OnClickLevel(int levelNum) {
+        currLevel = levelNum;
+        SceneManager.LoadScene(currLevel-1);
+    }
+
+    public void OnClickBack() {
+        this.gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
