1bcb013383c9a9e17e83b8deef3c3f6794f4e6cd
ShenceMYX <71965322+ShenceMYX@users.noreply.github.com>
2023-11-29 17:28:28 +0800

    Finished "new game" and "continue game" feature

--- prototype_6/Assets/HomeScreen.cs
+++ prototype_6/Assets/HomeScreen.cs
@@ -7,7 +7,21 @@ public class HomeScreen : MonoBehaviour
 {
     public void PlayGame()
     {
+        if (!PlayerPrefs.HasKey("level"))
+        {
+            PlayerPrefs.SetInt("level", 1);
+        }
         SceneManager.LoadScene("Level Selection");//load main menu
-        Debug.Log("load scene");
+    }
+
+    public void NewGame()
+    {
+        PlayerPrefs.SetInt("level", 1);
+        SceneManager.LoadScene("Level Selection");//load main menu
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
--- prototype_6/Assets/Scripts/LevelSelectionMenuManager.cs
+++ prototype_6/Assets/Scripts/LevelSelectionMenuManager.cs
@@ -2,29 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelectionMenuManager : MonoBehaviour
 {
     public static int currLevel;
-    public void OnClickLevel(int levelNum) {
+
+    [SerializeField] private Transform allLevelParent;
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("level"))
+            PlayerPrefs.SetInt("level", 1);
+
+        int currentLevel = PlayerPrefs.GetInt("level");
+        for (int i = allLevelParent.childCount - 1; i >= currentLevel; i--)
+        {
+            allLevelParent.GetChild(i).GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void OnClickLevel(int levelNum) 
+    {
         currLevel = levelNum;
         SceneManager.LoadScene(currLevel-1);
     }
 
-    public void OnClickBack() {
+    public void OnClickBack() 
+    {
         SceneManager.LoadScene("home_screen");
         //this.gameObject.SetActive(false);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
